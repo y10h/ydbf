25bb476ad365635dd165bc077ec507de0898d6d6
pythy <the.pythy@gmail.com>
2008-08-07 15:43:01 +0700

    More or less working ydbf.dump

--- ydbf/dump.py
+++ ydbf/dump.py
@@ -25,6 +25,17 @@ from optparse import OptionParser
 from ydbf import VERSION
 from ydbf.reader import YDbfStrictReader
 
+def _unescape_separator(option, opt_str, value, parser):
+    """
+    Unescape special symbols (like newline)
+    
+    When --rs '\n' passed to ydbf.dump, OptionParser saves it as
+    '\\n', i.e. escapes it. We want to unescape special symbols
+    like '\n', '\r', '\t' both in record and field separators
+    """
+    if value is not None:
+        value = value.replace('\\n', '\n').replace('\\r', '\r').replace('\\t', '\t')
+    setattr(parser.values, option.dest, value)
 
 def parse_options(args):
     """
@@ -33,12 +44,17 @@ def parse_options(args):
     parser = OptionParser(usage="%prog [options] files", version="%%prog %s" % VERSION)
     parser.add_option('-r', '--rs',
                            dest='record_separator',
-                           type='string',
+                           action='callback',
+                           callback=_unescape_separator,
                            default='\n',
+                           type='string',
                            help='output record separator [default newline]')
     parser.add_option('-f', '--fs',
                            dest='field_separator',
+                           action='callback',
+                           callback=_unescape_separator,
                            default=':',
+                           type='string',
                            help='output field separator [default colon]',
                            ),
     parser.add_option('-F', '--fields',
@@ -70,10 +86,16 @@ def parse_options(args):
     return options, args
 
 def csv_output_generator(data_iterator, record_separator, field_separator):
+    """
+    Make CSV-like output with specified record and field separators
+    """
     for rec in data_iterator:
         yield field_separator.join(str(f) for f in rec) + record_separator
 
 def table_output_generator(fields_spec, data_iterator):
+    """
+    Make table-look output
+    """
     # either separators do not acts on table output, this options only for keeping
     # interface similar to csv_output_generator.
     place_holders = []
@@ -110,6 +132,9 @@ def table_output_generator(fields_spec, data_iterator):
         yield format_string % tuple(rec)
 
 def _filter_fields(data_iterator, dbf_fields, fields_to_show):
+    """
+    Show only fields which listed in fields_to_show
+    """
     for rec in data_iterator:
         filtered_rec = tuple(
             value 
@@ -118,7 +143,30 @@ def _filter_fields(data_iterator, dbf_fields, fields_to_show):
         )
         yield filtered_rec
 
+def _escape_data(data_iterator, symbol_escape_to):
+    """
+    Escapes field separator in data
+    """
+    for rec in data_iterator:
+        yield tuple(str(x).replace(symbol_escape_to, '\\%s' % symbol_escape_to) for x in rec)
+
+def replace_null(data_iterator, undef):
+    """
+    Replace NULL (None) values by undef string
+    """
+    def provide_undef(v):
+        if v is None:
+            return undef
+        else:
+            return v
+    for rec in data_iterator:
+        yield tuple(provide_undef(x) for x in rec)
+    
+
 def dbf_data(fh, fields=None):
+    """
+    Return a fields spec and data generator
+    """
     # we don't want to use YDbfReader, because
     # we don't need either as_dict, no use_unicode
     reader = YDbfStrictReader(fh)
@@ -132,12 +180,18 @@ def dbf_data(fh, fields=None):
     return fields_spec, generator
 
 def write_output(output_fh, data_iterator, flush_on_each_record=True):
+    """
+    Write data from data_iterator to output_fh
+    """
     for rec in data_iterator:
         output_fh.write(rec)
         if flush_on_each_record:
             output_fh.flush()
 
 def dump(args):
+    """
+    Dump DBF file
+    """
     options, args = parse_options(args)
     if options.output:
         ofh = open(options.output, 'w')
@@ -146,11 +200,18 @@ def dump(args):
     for filename in args:
         fh = open(filename, 'rb')
         fields_spec, data_iterator = dbf_data(fh)
+        data_iterator = replace_null(data_iterator, options.undef)
         if options.table:
             output_generator = table_output_generator(fields_spec, data_iterator)
         else:
-            output_generator = csv_output_generator(data_iterator, options.record_separator, options.field_separator)
-        write_output(ofh, output_generator)        
+            data_iterator = _escape_data(data_iterator, options.field_separator)
+            output_generator = \
+                csv_output_generator(
+                        data_iterator,
+                        options.record_separator, 
+                        options.field_separator
+                    )
+        write_output(ofh, output_generator)      
         
 if __name__ == '__main__':
     dump(sys.argv[1:])
