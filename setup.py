90d0c3c34d817df3070765f8649abf0a9f419eb4
Scott Walton <scott.walton@mypebble.co.uk>
2018-01-12 10:20:09 +0000

    Add test setup for Circle

--- setup.py
+++ setup.py
@@ -1,25 +1,24 @@
 from setuptools import setup, find_packages
-import sys, os
 
-version = '0.3rc1'
+version = '0.4'
 
 setup(name='YDbf',
       version=version,
       description="Pythonic reader and writer for DBF/XBase files",
       long_description="""\
 YDbf is a library for reading/writing DBF files
-(also known as XBase) in pythonic way. It 
+(also known as XBase) in pythonic way. It
 represents DBF file as data iterator, where
 each record is a simple dict.
 .""",
       classifiers=[
-      'Development Status :: 3 - Alpha',
-      'Intended Audience :: Developers',
-      'License :: OSI Approved :: GNU General Public License (GPL)',
-      'Operating System :: OS Independent',
-      'Programming Language :: Python',
-      'Topic :: Database',
-      'Topic :: Software Development :: Libraries :: Python Modules',
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Database',
+          'Topic :: Software Development :: Libraries :: Python Modules',
       ],
       keywords='',
       author='Yury Yurevich',
@@ -29,11 +28,12 @@ each record is a simple dict.
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=True,
-      install_requires=[],
+      install_requires=[
+          'six'
+      ],
       entry_points="""
       # -*- Entry points: -*-
       [console_scripts]
       ydbfdump = ydbf.dump:main
       """,
       )
-      
